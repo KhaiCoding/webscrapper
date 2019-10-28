a10b4b35be1b71122827c3a9aa3e1f0bbfc6079f
KhaiCoding <KhaiCoding@github.com>
2019-10-28 20:57:12 +0900

    extract job title

---
+++ indeed.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+
+LIMIT = 50
+PYTHON_URL = f"https://www.indeed.com/jobs?as_and=python&limit={LIMIT}"
+
+
+def extract_indeed_pages():
+    python_result = requests.get(PYTHON_URL)
+    python_soup = BeautifulSoup(python_result.text, 'html.parser')
+    pagination = python_soup.find("div", {"class": "pagination"})
+    links = pagination.find_all('a')
+
+    pages = []
+    for link in links[0:-1]:
+        pages.append(int(link.string))
+
+    max_page = pages[-1]
+    return max_page
+
+
+def extract_indeed_jobs(last_page):
+    jobs = []
+
+    # for page in range(last_page):
+    python_result = requests.get(f"{PYTHON_URL}&start={0*LIMIT}")
+    python_soup = BeautifulSoup(python_result.text, 'html.parser')
+    python_results = python_soup.find_all(
+        "div", {"class": "jobsearch-SerpJobCard"})
+    for result in python_results:
+        title = (result.find("div", {"class": "title"})).find("a")["title"]
+        print(title)
+        # jobs.append(title)
+    return jobs
